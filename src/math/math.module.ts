42a14f253817233d7a5bb970913b7c04fc7e40c5
Fabricio <fabricio_montagnani@hotmail.com>
2022-12-26 12:03:35 -0300

    feat: updated Math module to provide Service classes to Interfaces

--- src/math/math.module.ts
+++ src/math/math.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { OperationsService } from './operation/operations.service';
 import { EquationService } from './equation/equation.service';
+import { IEquationSolver, IOperations } from './interfaces';
 
 @Module({
-  providers: [OperationsService, EquationService],
-  exports: [EquationService],
+  providers: [
+    OperationsService,
+    EquationService,
+    {
+      provide: IOperations,
+      useClass: OperationsService,
+    },
+    {
+      provide: IEquationSolver,
+      useClass: EquationService,
+    },
+  ],
+  exports: [
+    OperationsService,
+    EquationService,
+    {
+      provide: IOperations,
+      useClass: OperationsService,
+    },
+    {
+      provide: IEquationSolver,
+      useClass: EquationService,
+    },
+  ],
 })
 export class MathModule {}
